4713e062f5fb2763f6c6108d0a5b3f1869278954
sajidur rahaman <sajidur78@gmail.com>
2023-04-12 13:12:05 +0600

    Bring version down

--- Source/resource.h
+++ Source/resource.h
@@ -10,7 +10,7 @@
 #endif
 
 #define PRODUCT_NAME 				 "HE1 Mod Loader"
-#define VERSION_MAJOR 				 3
+#define VERSION_MAJOR 				 1
 #define VERSION_MINOR 				 0
 #define VERSION_REVISION 			 0
 #define VERSION_BUILD 				 0
