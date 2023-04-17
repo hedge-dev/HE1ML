1868314fe4e751c6c4e27939f701f2b4cab1b91f
sajidur rahaman <sajidur78@gmail.com>
2023-04-17 20:08:50 +0600

    Don't show error message on startup on unrecognised exes

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -27,7 +27,7 @@ void Startup()
 	{
 		char buffer[4096];
 		GetModuleFileNameA(MODULE_HANDLE, buffer, sizeof(buffer));
-		const char* message{ "Unsupported game detected." };
+		const char* message{ nullptr };
 		if (strstr(buffer, "slw.exe"))
 		{
 			message = "This version of slw.exe is not supported.\nPlease use the .exe released on January 6th 2016 (12.6 MB (13,315,072 bytes)).\nMD5: CEB45AF30E1E9341032DA0C68BB3A12D";
@@ -37,7 +37,10 @@ void Startup()
 			message = "This version of SonicGenerations.exe is not supported.\nPlease use the v1.0.0.5 .exe (23.1 MB (24,284,488 bytes)).\nMD5: FE5EA2725EE7FC51C35263D4E7C41721";
 		}
 
-		MessageBoxA(NULL, message, "HE1ML", MB_OK);
+		if (message != nullptr)
+		{
+			MessageBoxA(NULL, message, "HE1ML", MB_OK);
+		}
 		return;
 	}
 
