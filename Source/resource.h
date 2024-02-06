7265ee62890652a87a6c581408a3e7cf49b1974c
Sajid <sajidur78@gmail.com>
2024-02-06 23:03:36 +0600

    Version 0.10

--- Source/resource.h
+++ Source/resource.h
@@ -11,8 +11,8 @@
 
 #define PRODUCT_NAME 				 "HE1 Mod Loader"
 #define VERSION_MAJOR 				 0
-#define VERSION_MINOR 				 9
-#define VERSION_REVISION 			 8
+#define VERSION_MINOR 				 10
+#define VERSION_REVISION 			 0
 #define VERSION_BUILD 				 0
 
 #define TOSTRING2(ARG) #ARG
